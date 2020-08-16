022c8f93e22b002687ec02236a8d271d21768489
chboo1 <samuel.navert@gmail.com>
2020-08-16 16:08:03 -0400

    FSM Start

--- dino.py
+++ dino.py
@@ -91,6 +91,8 @@ class Hole(Element):
 
 class Main():
     def __init__(self, world):
+        self.inputs = []
+        self.state = Stand()
         self.worldDir = "{}/data".format(os.path.dirname(os.path.realpath(__file__)))
         self.start()
 
@@ -99,28 +101,29 @@ class Main():
         self.width = self.master.winfo_screenwidth()
         self.levels = []
         self.height = self.master.winfo_screenheight() - 55
+        self.fsm = FSM(self.height - 50, state=self.state)
         self.master.geometry("{}x{}".format(self.width, self.height))
         self.canvas = Canvas(self.master, width=self.width, height=self.height)
         self.text = self.canvas.create_text(500, 100, fill="#000000", text="", anchor="w")
         self.pos = 130
         i = 0
-        for f in os.listdir(self.worldDir):
-            if not f[0] == "." or f[-4:0] == ".swp" or f[-6:0] == ".ignore":
-                self.levels.append([f, None])
-        for level in self.levels:
-            self.levels[i][1] = self.canvas.create_rectangle(0, self.pos, self.width, self.pos + 30, fill="#b55a3a", outline="#555555")
-            self.canvas.create_text(self.width / 2, self.pos + 15, text=level[0])
-            self.pos += 30
-            me = self.levels[i - 1][1]
-            # self.canvas.tag_bind(me, "<Button-1>", lambda e: self.setworldid(i))
-            i += 1
+        #for f in os.listdir(self.worldDir):
+        #    if not f[0] == "." or f[-4:0] == ".swp" or f[-6:0] == ".ignore":
+        #        self.levels.append([f, None])
+        #for level in self.levels:
+        #    self.levels[i][1] = self.canvas.create_rectangle(0, self.pos, self.width, self.pos + 30, fill="#b55a3a", outline="#555555")
+        #    self.canvas.create_text(self.width / 2, self.pos + 15, text=level[0])
+        #    self.pos += 30
+        #    me = self.levels[i - 1][1]
+        #    self.canvas.tag_bind(me, "<Button-1>", lambda e: self.setworldid(i))
+        #    i += 1
                         
         self.started = False
         self.worldId = ""
         self.canvas.pack()
         self.master.bind("<KeyPress>", self.key)
-        self.master.bind("<4>", lambda e: self.canvas.move("all", 0, 5))
-        self.master.bind("<5>", lambda e: self.canvas.move("all", 0, -5))
+        #self.master.bind("<4>", lambda e: self.canvas.move("all", 0, 5))
+        #self.master.bind("<5>", lambda e: self.canvas.move("all", 0, -5))
         self.master.after(16, self.afterloop)
         self.master.mainloop()
         try:
@@ -158,10 +161,11 @@ class Main():
         self.character = self.c.create_oval(50, self.height - (50 + int(self.charSize)), 50 + int(self.charSize), self.height - 50, fill=self.charColor, outline=self.charColor)
         self.generate_world()
         self.c.pack()
+        self.inputs = []
+        print(self.afterloop)
         self.root.after(16, self.afterloop)
         self.root.bind("<KeyPress>", self.key)
         self.root.bind("<KeyRelease>", self.keyRelease)
-        self.root.bind("<space>", self.jump)
         self.root.bind("<Escape>", self.kr)
         self.root.mainloop()
 
@@ -169,14 +173,12 @@ class Main():
 
     def key(self, event=None):
         if self.started:
-            if event.keysym == "Left":
-                self.movingleft = True
-            elif event.keysym == "Right":
-                self.movingright = True
-            elif event.char == "n":
-                self.c.move("all", -15, 0)
-                self.c.move(self.character, 15, 0)
-                self.recoil += 15
+            if event.keysym == "a":
+                self.inputs.append("LEFT")
+            if event.keysym == "d":
+                self.inputs.append("RIGHT")
+            if event.keysym == "space":
+                self.inputs.append("JUMP")
         else:
             if event.keysym == "Return":
                 self.started = True
@@ -188,10 +190,12 @@ class Main():
 
 
     def keyRelease(self, event=None):
-        if event.keysym == "Left":
-            self.movingleft = False
-        elif event.keysym == "Right":
-            self.movingright = False
+        if event.keysym == "a":
+            self.inputs.remove("LEFT")
+        if event.keysym == "d":
+            self.inputs.remove("RIGHT")
+        if event.keysym == "space":
+            self.inputs.remove("JUMP")
 
 
     def kr(self, event=None):
@@ -217,88 +221,77 @@ class Main():
             self.landingX.append([obj.mycoords()[0], obj.mycoords()[1]])
 
 
-
-    def jump(self, event=None):
-        self.jumpcurrentheight = 0
-        if not self.jumping and not self.doublejumpdone:
-            if self.inair:
-                self.doublejumpdone = True
-                self.jumping = True
-            self.inair = True
-            self.currentjumpheight = 0
-            self.jumping = True
-   
-
     def afterloop(self):
+        # flag AFTERLOOP
         if self.started:
-            if self.c.coords(self.character)[1] >= self.height:
-                self.c.create_rectangle(0, 0, self.width, self.height, fill="#ff0000", outline="#ff0000")
-                self.c.create_text(self.width / 2, self.height / 2, fill="#000000", text="Game Over.", font=("Helvetica", 40))
-                self.c.update()
-                time.sleep(3)
-                self.kr()
-                sys.exit(0)
-            self.num, self.canLand = self.arr[self.num].check(self.num, charPos=[self.c.coords(self.character)[0] + self.recoil, self.c.coords(self.character)[2] + self.recoil])
             self.c.move("all", -1, 0)
-            self.c.move (self.character, 1, 0)
-            self.recoil += 1
-            if self.jumping and self.jumpcurrentheight < 20:
+            self.c.move(self.character, 1, 0)
+            self.state = self.fsm.getState(self.inputs, self.state)
+            if self.state.value == "MOVE":
+                self.c.move(self.character, 20 * self.state.direction, 0)
+            if self.state.value == "JUMP":
                 self.c.move(self.character, 0, -10)
-                self.jumpcurrentheight += 1
-            elif self.inair:
-                self.c.move(self.character, 0, 5)
-            if (self.c.coords(self.character)[3] in self.landingY) and self.canLand:
-                self.inair = False
-                self.doublejumpdone = False
-            elif not self.canLand:
-                if not self.inair:
-                    self.jumpcurrentheight = 20
-                    self.inair = True
-                elif self.inair:
-                    pass
-            if self.jumpcurrentheight >= 20:
-                self.jumping = False
-            if self.movingleft:
-                self.c.move(self.character, -5 ,0)
-                self.pos = max(self.c.coords(self.character)[0], 0)
-                self.pos2 = self.pos + self.charSize
-                self.posy=[self.c.coords(self.character)[1], self.c.coords(self.character)[3]]
-                self.c.coords(self.character, self.pos, self.posy[0], self.pos2, self.posy[1])
-            if self.movingright:
-                self.c.move(self.character, 5 ,0)
-                self.pos2 = min(self.c.coords(self.character)[2], self.width)
-                self.pos = self.pos2 - self.charSize
-                self.posy=[self.c.coords(self.character)[1], self.c.coords(self.character)[3]]
-                self.c.coords(self.character, self.pos, self.posy[0], self.pos2, self.posy[1])
-            self.win = self.arr[-1].check(self.num, charPos=[self.c.coords(self.character)[0] + self.recoil, self.charSize])
-            if not self.win:
-                self.root.after(16, self.afterloop)
-            else:
-                self.c.create_rectangle(0, 0, self.width, self.height, fill="#ffff00", outline="#ffff00")
-                self.c.create_text(self.width / 2, self.height / 2, fill="#00ff00", text="You Win!!", font=("Helvetica", 40))
-                self.c.update()
-                time.sleep(3)
-                self.kr()
+            if self.state.value == "FALL":
+                self.c.move(self.character, 0, 2)
+            self.root.after(16, self.afterloop)
         else:
             self.canvas.itemconfig(self.text, text=self.worldId)
             self.master.after(16, self.afterloop)
 
 
-    def flag(self, element, num, side):
-        if side == "right":
-            if element == "hole":
-                self.islandingX = False
-                self.currentX["type"] = "Hole"
-                self.currentX["num"] = num
-            elif element == "platform":
-                self.islandingX = True
-                self.currentX["type"] = "Platform"
-                self.currentX["num"] = (num + 1)
+class FSM():
+    #flag FSM
+    def __init__(self, groundLevel=100, state=None):
+        self.state = state
+        self.groundLevel = groundLevel
 
 
-    def setworldid(self, num):
-        self.worldId = self.levels[int("{}".format(num - num))][0]
-        self.master.destroy()
+    def getState(self, inputs,  state,  pos=[]):
+        self.pos = pos
+        self.inputs = inputs
+        self.state = state
+        if self.inputs:
+            if "LEFT" in self.inputs:
+                return Move(direction=-1)
+            if "RIGHT" in self.inputs:
+                return Move(direction=1)
+            if not "LEFT" in self.inputs and not "RIGHT" in self.inputs and not "JUMP" in self.inputs and not self.state.value == "FALL":
+                return Stand()
+            if "JUMP" in self.inputs:
+                return Jump()
+            if self.pos[3] < self.groundLevel and not self.state.value == "JUMP":
+                return Fall()
+            else:
+                return Stand()
+        else:
+            return Stand()
+
+
+class Stand():
+    #flag STAND
+    def __init__(self):
+        self.value = "STAND"
+
+
+class Move():
+    #flag MOVE
+    def __init__(self, direction=0):
+        self.value="MOVE"
+        self.direction = direction
+
+
+class Jump():
+    #flag JUMP
+    def __init__(self):
+        self.value = "JUMP"
+
+
+class Fall():
+    #flag FALL
+    def __init__(self):
+        self.value = "FALL"
+
+
 print("Defined all classes")
 
 print("Starting up")
